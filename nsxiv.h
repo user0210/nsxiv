b2f1b2c4ece1173ec88d95e4d679d0f4a965e115
philor1 <scheibe.philipp@gmail.com>
2022-06-11 16:38:21 +0200

    patch-alpha [https://github.com/nsxiv/nsxiv-extra/tree/master/patches/alpha (alpha-c627537.patch, 5.10.2021 + see commit f05165)]

--- nsxiv.h
+++ nsxiv.h
@@ -230,6 +230,7 @@ Imlib_Image img_open(const fileinfo_t*);
 #if HAVE_LIBEXIF
 void exif_auto_orientate(const fileinfo_t*);
 #endif
+void render_core(win_t*, int, int, int, int, int, int, int, int, bool);
 
 
 /* options.c */
@@ -399,9 +400,11 @@ struct win {
 	Window xwin;
 	win_env_t env;
 
-	XColor win_bg;
+	XColor win_bg; /* pre-multiplied alpha */
+	XColor win_bg_postmul; /* post-multiplied alpha */
 	XColor win_fg;
 	XColor mrk_fg;
+	unsigned int win_alpha;
 #if HAVE_LIBFONTS
 	XftColor bar_bg;
 	XftColor bar_fg;
